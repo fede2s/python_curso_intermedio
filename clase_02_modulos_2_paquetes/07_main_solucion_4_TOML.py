c1dee34fb30470096cf4419cd4cc19955021fde5
Fede <fede.2santos_96@hotmail.com>
2025-08-23 17:21:28 -0300

    aclaracion toml

--- clase_02_modulos_2_paquetes/07_main_solucion_4_TOML.py
+++ clase_02_modulos_2_paquetes/07_main_solucion_4_TOML.py
@@ -1,6 +1,14 @@
 """
 archivo facil de leer de configuracion con clave = valor
-se parece al diccionario o al formato json desde un punto de vista
+se parece al diccionario o al formato json desde un punto 
+de vista
 
-para la configuracion y creacion de modulo que se instale dentro de site-packages
+para la configuracion y creacion de modulo que se instale 
+dentro de site-packages
+
+El archivo lleva un nombre específico:
+    pyproject.toml
+
+En la página de setuptools tenemos un ejemplo para tomar la 
+configuración base para un proyecto
 """
